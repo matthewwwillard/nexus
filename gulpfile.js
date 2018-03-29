ec610d24dd5385e55ab5ba73b78e8189f25a5b9b
Matthew William Willard <matthewwwillard@gmail.com>
2018-03-29 10:14:29 -0400

    Gulp watch now works

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
+var run = require('gulp-run-command').default;
 var ts = require("gulp-typescript");
 var tsProject = ts.createProject("./tsconfig.json");
 var del = require('del');
 var runSequence = require('run-sequence');
+var gls = require('gulp-live-server');
+
 gulp.task("typescript", function()
 {
     return tsProject.src()
@@ -13,7 +16,23 @@ gulp.task("clean", function()
 {
     return del("./dist",{force:true});
 });
+gulp.task("start", run("node dist/startApi"));
 gulp.task("default", function(callback)
 {
     runSequence('clean','typescript', callback);
+});
+gulp.task('watch',['default'], function(){
+
+    var server = gls.new('dist/startApi.js');
+    server.start();
+    gulp.watch('src/core/**/*.ts', function(file){
+        console.log('Watch!');
+        runSequence('clean','typescript', function () {
+            server.stop().then(function () {
+                server.start();
+            })
+        });
+    });
+
+
 });
